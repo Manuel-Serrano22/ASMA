90b34d39f90022b1659b4e4cc711e9748825dde1
up202402793 <up202402793@edu.fe.up.pt>
2025-03-27 19:55:27 +0000

    some additional functions

--- utils.py
+++ utils.py
@@ -13,6 +13,7 @@ def read_nodes_from_csv(filepath):
             })
     return nodes
 
+
 def build_distance_dict(nodes):
     distance = {}
 
@@ -29,8 +30,51 @@ def build_distance_dict(nodes):
                 distance[id_i][id_j] = math.hypot(dx, dy)
     return distance
 
+
+def print_distance_matrix(distance_dict):
+    ids = list(distance_dict.keys())
+    print(f"{'':10}", end="")
+    for id_j in ids:
+        print(f"{id_j:10}", end="")
+    print()
+    for id_i in ids:
+        print(f"{id_i:10}", end="")
+        for id_j in ids:
+            print(f"{distance_dict[id_i][id_j]:10.2f}", end="")
+        print()
+
+
 def get_distance_between_agents(agent1_id, agent2_id, distance_dict):
     try:
         return distance_dict[agent1_id][agent2_id]
     except KeyError:
         raise ValueError("One or both agent IDs not found in the distance dictionary.")
+
+
+def get_node_closest_to_center(nodes):
+    avg_x = sum(node['x'] for node in nodes) / len(nodes)
+    avg_y = sum(node['y'] for node in nodes) / len(nodes)
+
+    street_nodes = [node for node in nodes if node['id'].startswith('street')]
+
+    min_distance = float('inf')
+    closest_node = None
+
+    for node in street_nodes:
+        dx = node['x'] - avg_x
+        dy = node['y'] - avg_y
+        dist = math.hypot(dx, dy)
+
+        if dist < min_distance:
+            min_distance = dist
+            closest_node = node
+
+    return closest_node
+
+
+def get_distance_truck_to_bin(truck_position, bin_position):
+
+    dx = truck_position[0] - bin_position[0]
+    dy = truck_position[1] - bin_position[1]
+    
+    return math.hypot(dx, dy)
